2b7dac68386ab8cdb9625bde3c3f23a396048a40
hemfreitas11 <hemfreitas11@users.noreply.github.com>
2021-09-19 14:21:49 -0300

    Organized project structure

--- src/main/java/me/bkrmt/bkcore/textanimator/animations/RGBRainbowAnimation.java
+++ src/main/java/me/bkrmt/bkcore/textanimator/animations/RGBRainbowAnimation.java
@@ -3,8 +3,9 @@ package me.bkrmt.bkcore.textanimator.animations;
 import me.bkrmt.bkcore.textanimator.AnimationResponse;
 import me.bkrmt.bkcore.textanimator.TextAnimation;
 import me.bkrmt.bkcore.textanimator.TextAnimator;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
+import net.md_5.bungee.api.ChatColor;
+
+import java.awt.*;
 
 public class RGBRainbowAnimation implements TextAnimation {
     private final boolean bold;
@@ -17,7 +18,8 @@ public class RGBRainbowAnimation implements TextAnimation {
 
     @Override
     public AnimationResponse processText(TextAnimator animator, int index, int currentPause) {
-        StringBuilder frame = new StringBuilder(Color.fromRGB(index) + ChatColor.stripColor(animator.getInitialText()));
+        System.out.println(animator.getUpdateFrequency());
+        StringBuilder frame = new StringBuilder(ChatColor.of(new Color(index)) + ChatColor.stripColor(animator.getInitialText()));
         if (index >= 2000) index = 0;
         else index++;
         return new AnimationResponse(index, currentPause, frame.toString());
